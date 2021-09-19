e3463ab44edb93d2965321fe0d6f7f5a279286b7
Thoutain <qianshan.yan@gmail.com>
2021-09-19 13:21:42 +0800

    Update lc15三数之和.cpp

--- 双指针/lc15三数之和.cpp
+++ 双指针/lc15三数之和.cpp
@@ -35,6 +35,39 @@ public:
 };
 */
 
+// right version
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> res;
+        if (nums.size() < 3)
+            return res;
+        int l = 1;
+        int r = nums.size() - 1;
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < nums.size() - 2; i ++) {
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+            l = i + 1;
+            r = nums.size() - 1;
+            
+            while (l < r) {
+                int sum = nums[l] + nums[r];
+                if (nums[i] == -sum) {
+                    res.push_back({nums[i], nums[l], nums[r]});
+                    l ++;
+                    while (l < r && nums[l] == nums[l - 1])
+                        l ++;
+                }
+                else if (nums[i] > -sum)
+                    r --;
+                else if (nums[i] < -sum)
+                    l ++;
+            }
+        }
+        return res;
+    }
+};
 int main() {
 
 
